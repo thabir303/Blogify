d45eca542ecb416a40a9c7e9b0af73800cf5578e
thabir303 <bsse1321@iit.du.ac.bd>
2025-04-14 02:21:25 +0600

    add allowed origin for production environment in CORS settings

--- backend/blogify/blogify/settings.py
+++ backend/blogify/blogify/settings.py
@@ -147,6 +147,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
+    'https://blogify-y1xu.onrender.com'
     'http://localhost:5173',
 ]
 
