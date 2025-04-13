8458758e9674ab8090e3cedae8aaf4a96c733b60
thabir303 <bsse1321@iit.du.ac.bd>
2025-04-14 03:00:39 +0600

    add missing comma to CORS_ALLOWED_ORIGINS in settings.py

--- backend/blogify/blogify/settings.py
+++ backend/blogify/blogify/settings.py
@@ -147,7 +147,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    'https://blogify-y1xu.onrender.com'
+    'https://blogify-y1xu.onrender.com',
     'http://localhost:5173',
 ]
 
